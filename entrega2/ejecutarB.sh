138ae429d2272be63e14e6a56c7a7343063c9a66
Baruffaldi Juan Manuel <baruffaldi.jm@gmail.com>
2016-04-29 10:33:48 -0300

    agregado archivos mios

---
+++ entrega2/ejecutarB.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#make
+#./tiger ../tests/good/sumatest.tig > /home/jbaruffaldi/public_html/salida.txt
+gcc -c file.s -ggdb -Wall -Wno-unused-function -Wunused-result -Wno-unused-result
+gcc -o file file.o
+
